2f491b50ca1a4de4fce8122ea91ddf83c3693cb6
Elicia Durtnall <eliciadurtnall@gmail.com>
2014-11-20 00:35:08 -0500

    minor changes back

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -16,11 +16,7 @@ jQuery(function($) {
       
       socket.emit('message', date);
       socket.emit('message', text);
-      socket.emit('message', image);
-    document.getElementsByClassName("messages").innerHTML = newHTML;
-    newHTML.setAttribute("class", "msgStyle");
-
-        
+      socket.emit('message', image);     
 
     });
 
